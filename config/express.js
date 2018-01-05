4a0743b7b6a5d24d4246ff9d45c2832151cb01c9
Daniel Andrade <dca650@gmail.com>
2018-01-05 00:53:43 -0300

    fix signup username issue

--- config/express.js
+++ config/express.js
@@ -31,6 +31,23 @@ module.exports = function(){
         .into(app);
 
 
+   //  app.use(function(req, res, next){
+   //      if (process.env.NODE_ENV == "production") {
+   //          res.status(404).render("errors/404");
+   //          next();
+   //      }else{
+   //          next();
+   //      }
+   //  });
+   //
+   //  app.use(function(error, request, response, next){ //Called first if error occurs
+   //      if (process.env.NODE_ENV == "production") {
+   //          response.status(500).render("errors/500");
+   //          return;
+   //      }
+   //      next(error);
+   // });
+
     app.consts = {}
     app.consts.districts = ['Nenhum','35º BI', 'Aeroporto', 'Asa Branca', 'Aviário', 'Baraúnas', 'Brasília', 'Calumbi', 'Campo Limpo', 'Caseb', 'Capuchinhos', 'Campo do Gado Novo', 'Chácara São Cosme', 'Cidade Nova', 'CIS', 'Conceição', 'Feira IV', 'Feira V', 'Feira VI', 'Feira VII', 'Feira IX', 'Feira X', 'Gabriela', 'Jardim Acácia', 'Jardim Cruzeiro', 'Lagoa Grande', 'Limoeiro', 'Muchila', 'Nova Esperança', 'Novo Horizonte', "Olhos D'Água", 'Papagaio', 'Parque Getúlio Vargas', 'Parque Ipê', 'Pedra do Descanso', 'Ponto Central', 'Queimadinha', 'Rua Nova', 'Santa Mônica', 'Santo Antônio dos Prazeres', 'São João', 'Sobradinho', 'Sim', 'Subaé']
 
